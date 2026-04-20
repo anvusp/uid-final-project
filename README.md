92a242b5f0bd6c164942ef83c434a3a5f6bd62bb
Maira Molina <mpmolina1320@gmail.com>
2026-04-19 23:47:58 -0400

    Dynamicly updating lesson and quiz page. Started json file

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # uid-final-project
-hello world
+
+## First Priorities
+|- need to store the user's answers
+|- what time a user enters a page
+|- Quiz results page & backend logic
+|- fix links in navigation bar
+|- Logic for quizes -> make the map section clickable
+|- need a logic to find the what the next url is
+    |- data.json has a timeline that specifies if the page is a lesson, question, or transition & corresponding id
+    |- Idea: Have a function that parses the timeline and then renders appropriate url and content
+|- transition template
+
+## Second Priorities
+|- update description on lessons
+|- Make sure audio files are correct for each lesson
+|- Enable/disable the next button until user answers question correctly
+|- update alt for image descriptions
+
+
+## Questions
+- Update backend?
+- What should the transition urls be?
--- server.py
+++ server.py
@@ -1,12 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json
+import os
 app = Flask(__name__)
+
+# Load Data
+filename = os.path.join(app.static_folder, 'data.json')
+with open(filename) as data_file:
+  data = json.load(data_file)
+LESSONS = {lesson["id"]: lesson for lesson in data["lessons"]}
+QUESTIONS = {question["id"]: question for question in data["questions"]}
+TRANSITIONS = {transition["id"]: transition for transition in data["transitions"]}
+TIMELINE = data["timeline"]
+
 
 # Route 1: Home Page -------------------------------------------------------
 @app.route('/')
 def render_homepage():
-  return render_template('homepage.html')
+  return render_template('homepage.html', lesson = LESSONS[1])
 
 # Route 1: Learn Page -------------------------------------------------------
-@app.route('/learn')
-def render_learnpage():
-  return render_template('learn.html')
+@app.route('/learn/<int:id>')
+def render_learnpage(id=None):
+  lesson = LESSONS.get(id)
+  if not lesson: 
+    return "Lesson not found", 404
+  
+  return render_template('learn.html', lesson=lesson)
+
+# Route 2: Quiz Page 
+@app.route('/quiz/<int:id>')
+def render_quizpage(id=None):
+  question = QUESTIONS.get(id)
+  if not question: 
+    return "Question not found", 404
+  
+  return render_template('quiz.html', question=question)
