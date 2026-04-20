abc8641018587980a4233303af1b6f3bf25e4029
Maira Molina <mpmolina1320@gmail.com>
2026-04-20 11:46:11 -0400

    Updated routes, templates, and css elements

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ TIMELINE = data["timeline"]
 # Route 1: Home Page -------------------------------------------------------
 @app.route('/')
 def render_homepage():
-  return render_template('homepage.html', lesson = LESSONS[1])
+  return render_template('transition.html', transition = TRANSITIONS[1])
 
 # Route 2: Learn Page -------------------------------------------------------
 @app.route('/learn/<int:id>')
@@ -34,3 +34,17 @@ def render_quizpage(id=None):
     return "Question not found", 404
   
   return render_template('quiz.html', question=question)
+
+# Route 4: Transition Pages -------------------------------------------------------
+@app.route('/transition/<int:id>')
+def render_transitionpage(id=None):
+  transition = TRANSITIONS.get(id)
+  if not transition:
+    return "Transition not found", 404
+
+  return render_template('transition.html', transition=transition)
+
+# Route 5: Quiz Results Page -------------------------------------------------------
+@app.route('/results')
+def render_results():
+  return render_template('quiz_results.html')
